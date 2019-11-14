a6e98eb85a60aac86d63d62dd8736e6921447e1d
itscatherinelee <itscatherinehlee@gmail.com>
2019-11-14 13:05:47 -0500

    understand how parser works lmao

--- Emoji.py
+++ Emoji.py
@@ -1,12 +1,29 @@
 import argparse # parser for command-line options
-from characters import CHARACTERS
+from Characters import CHARACTERS
 
-def convert_str_to_emoji(input, emoji):
-	input.
+# def convert_arr_to_str(character, string, space):
+# 	output_str = ' '
+# 	for row in arr:
+# 		for col in row:
+# 			output_str += string if col else space
+# 		output_str += '\n'
+# 	return output_str
+
+def convert_str_to_emoji(input_str, input_emoji):
+	input_str = input_str.lower() #maybe in the future i would do uppercase and lowercase
+	#want to have all the strings that are input turn into the arrays
+	output_emoji = ' '
+	return output_emoji
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description="Convert your string into emojis")
+	parser.add_argument('text', help='The text to emoji-fy')
+	parser.add_argument('emoji', help='The choosen emoji')
+	# parser.add_argument('-e', '--emojis')
+	
 	args = parser.parse_args()
+	#stringInput, emojiNameString
 	try:
-		output = convert_str_to_emoji(args.text, emojis=args.emojis)
-		print(output)
+		output = convert_str_to_emoji(args.text, args.emoji)
+	except ValueError as e:
+		print("catherine doesnt want to deal with this right now")
