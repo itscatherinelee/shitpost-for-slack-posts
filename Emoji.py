45b348ecda9e05bb73af8cb53287018523052b49
itscatherinelee <itscatherinehlee@gmail.com>
2019-11-15 13:22:06 -0500

    it works :)

--- Emoji.py
+++ Emoji.py
@@ -1,20 +1,29 @@
 import argparse # parser for command-line options
 from Characters import CHARACTERS
-#https://github.com/shrugs/partyparrot/blob/master/partyparrot.py
 
-def convert_arr_to_character(input_character):
+def convert_arr_to_character(input_character, input_emoji):
+	if input_character == ' ':
+		return '\n\n\n\n\n\n\n\n'
+	arr = CHARACTERS[input_character]
 	output_character = ' '
 	for row in arr:
 		for col in row:
-			if (col == '1'):
-				output_character += input_character
+			if (col == 1): 
+				output_character += input_emoji
+			else:
+				output_character += "     "
 		output_character += '\n'
+	output_character += '\n\n'
 	return output_character
 
 def convert_str_to_emoji(input_str, input_emoji):
 	input_str = input_str.lower() #maybe in the future i would do uppercase and lowercase
 	#want to have all the strings that are input turn into the arrays
+	
+	#disect the string into characters and convert_arr_to_character(input_characters)
 	output_emoji_str = ' '
+	for character in input_str:
+		output_emoji_str += convert_arr_to_character(character, input_emoji)
 	return output_emoji_str
 
 if __name__ == "__main__":
@@ -27,5 +36,6 @@ if __name__ == "__main__":
 	#stringInput, emojiNameString
 	try:
 		output = convert_str_to_emoji(args.text, args.emoji)
+		print(output)
 	except ValueError as e:
 		print("catherine doesnt want to deal with this right now")
